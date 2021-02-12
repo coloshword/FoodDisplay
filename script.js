0f09d37f98bde0038752b3b20424d41a47623f4e
coloshword <aceliang2001@gmail.com>
2021-02-12 16:05:02 -0500

    white background with blue banner

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-function displayBreakfast() {
-    let request = new XMLHttpRequest();
-    request.open('GET', 'http://172.24.49.229:8080/breakfast', true);
-    let breakfast;
-    request.onload = () => {
-        breakfast = JSON.parse(request.response);
-        console.log(breakfast);
+var foods = {
+    "Scrambled Eggs": {
+        "Calories" : "50",
+        "Protein" : "5g"
+    },
+    "Omelette" : {
+        "Calories" : "150",
+        "Protein" : "25g"
+    },
+    "Sausage" : {
+        "Calories" : "450",
+        "Protein" : "30g"
+    },
+    "Fruit Cup" : {
+        "Calories" : "120",
+        "Protein" : "0g"
+    }
+}
+function displayFood(meal) {
+    var addText = document.querySelector(".putFood");
+    let h4 = document.createElement("h4");
+    h4.setAttribute("style", "text-align: center; color: rgb(235, 155, 76)");
+    h4.appendChild(document.createTextNode("<<" + meal + ">>"));
+    addText.appendChild(h4);
+    var foodContainer = document.createElement("div");
+    foodContainer.classList = "itemBox";
+    addText.appendChild(foodContainer);
+    for(var key in foods) {
+        item = foods[key];
+        let displayBox = document.createElement("div");
+        displayBox.classList = "displayBox"
+        let ribbon = document.createElement("div");
+        ribbon.classList = "topRibbon";
+        let h6 = document.createElement("h6");
+        h6.setAttribute("style", "text-align: center;")
+        h6.appendChild(document.createTextNode(key + "\n" + item))
+        displayBox.appendChild(ribbon);
+        displayBox.appendChild(h6);
+        foodContainer.appendChild(displayBox)
     }
 }
 
-displayBreakfast()
+displayFood("Breakfast");
